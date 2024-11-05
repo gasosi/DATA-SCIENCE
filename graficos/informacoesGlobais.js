7460001df8e1952cf8702aa5f8693ef906130bc2
Gabriel Soares <115032490+gasosi@users.noreply.github.com>
2024-11-05 07:40:54 -0300

    SADAS

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,7 +6,9 @@ const dados = await resposta.jason()
 console.log(dados)
 
 const paragrafo = document.createElement('p')
-paragrafo.classList.add('graficos-container__texto')
+paragrafo.classList.add
+('graficos-container__texto')
+
 paragrafo.innerHTML = `De <span> ${dados.total_pessoas_mundo} </span> bilhões de pessoas no planeta, existem aproximadamente <span> ${dados.total_de_pessoas_com_acesso_a_educacao} </span> bilhões de pessoas com acesso à educação, onde o tempo médio de estudo é de 4 horas por dia. Como Maria sempre pensou em estudar porém assim como 1,88 bilhão de pessoas sem acesso a educação, enfrentava muitas dificuldades. Para isso ela começou a se dedicar estudando diariamente e se tornou parte da parcela de <span> ${dados.total_pessoas_com_educacao_superior} </span> bilhão de pessoas que conseguem concluir o ensino superior.`
 
 const container = document.getElementById('graficos-container')
